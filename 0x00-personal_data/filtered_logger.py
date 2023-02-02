12e47fce1f9ea23af5c9c2e23017c53bbe6dfc9e
kinyarasam <skinyara.30@gmail.com>
2023-02-02 19:51:42 +0300

    1. Log formatter
    Update class to accept a list of strings fields constructor argument
    Implement the format method to filter values in incoming log records using filter_datum
    FIX: pycodestyle, type annotation and PEP8 guidelines
    
    Fixes: #none

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,21 +4,45 @@
 """
 import re
 from typing import List, Union
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
 
 
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
-def filter_datum(fields: List[str], redaction: str,
-        message: str, separator: str) -> str:
+
+
+def filter_datum(
+        fields: List[str], redaction: str,
+        message: str, separator: str
+        ) -> str:
     """Obfuscate Log message fields.
 
     Args:
         fields (List[str]): The fields to obfuscate.
         redaction (str): The string to replace the fields with.
         message (str): The log line to process.
-        separator (str): The character that separates the fields in the log line.
+        separator (str): The character that separates the fields in the
+            log line.
 
     Returns:
         str: The obfuscated log line.
