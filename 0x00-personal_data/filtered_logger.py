11a88f7ae4c1f15acc49e6f104e545730d41efc3
kinyarasam <skinyara.30@gmail.com>
2023-02-04 12:53:25 +0300

    Read and filter data
    Implement a main function that takes no argument and returns nothing
    Update the main.sql file to insert data to the database.
    
    Fixes: #none

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -88,3 +88,29 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
     logger.addHandler(stream_handler)
     return logger
+
+
+def main():
+    """Logs the information about a user records in a table.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                    lambda x: '{}={}'.format(x[0], x[1]),
+                    zip(columns, row),
+                )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
+
+
+if __name__ == '__main__':
+    main()
