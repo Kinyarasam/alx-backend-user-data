d6af194b4e163b711d52db138a37b0bb382554ba
kinyarasam <skinyara.30@gmail.com>
2023-02-08 16:51:11 +0300

    1. Error handler: Unauthorized
    Add a new error handler for the status code 401 using the jsonify from Flask
    add a new endpoint in 'api/v1/views/index.py' to test the new error handler.
    
    Fixes #none

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
+    """ Test the 401 error handler
+    Return:
+        - 401 Unauthorized error
+    """
+    abort(401)
